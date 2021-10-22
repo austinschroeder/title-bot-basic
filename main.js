84cffd0dfb63129232505b0ad2cfb5841956af81
austinschroeder <austinsoundy@gmail.com>
2021-10-21 17:40:12 -0700

    cleaning up comments

--- main.js
+++ main.js
@@ -4,12 +4,6 @@ const button = document.getElementById("searchBtn");
 const result = document.getElementById("title");
 const titleApi = "http://127.0.0.1:4000/title-bot";
 
-// const siteTitle = ''
-
-// const getTitle = (title) => {
-
-// }
-
 button.addEventListener("click", async (event) => {
   const query = input.value;
   const returnTitle = await fetch(titleApi, {
