2e8f008b8ed060d2e6eee3d45ce86b31cf5745c1
austinschroeder <austinsoundy@gmail.com>
2021-10-22 18:05:00 -0700

    functionality is working

--- main.js
+++ main.js
@@ -2,21 +2,28 @@
 const input = document.getElementById("searchUrl");
 const button = document.getElementById("searchBtn");
 const result = document.getElementById("title");
+
 const titleApi = "http://127.0.0.1:4000/title-bot";
 
 button.addEventListener("click", async (event) => {
-  const query = input.value;
+  const query = { URL: input.value };
+  console.log(query);
   const returnTitle = await fetch(titleApi, {
     method: "POST",
-    body: query,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(query),
   }).then(async (response) => {
     const data = await response.text();
-    console.log(data);
+    console.log(data.data);
+
+    const p = document.createElement("p");
+    p.innerText = returnTitle;
+    document.appendChild(p);
+
     return data;
   });
-
-  const p = document.createElement("p");
-  p.innerText = returnTitle;
-  document.appendChild(p);
 });
-// "info:siteurl"
+
+document.querySelector("title");
--- server.js
+++ server.js
@@ -2,22 +2,51 @@ const fetch = require("isomorphic-fetch");
 const path = require("path");
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const got = require("got");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+// Middleware
+//const bodyParser = require("body-parser"); //allows req.body.. depricated?
+const corsOptions = {
+  origin: "*",
+};
+app.use(cors(corsOptions));
 
 //Routes
 app.use(express.static(__dirname)); //https://stackoverflow.com/questions/19620239/cant-get-index-html-to-show-with-express-in-nodejs
-
+//app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
 app.post("/title-bot", async (req, res) => {
-  const providedUrl = req.body; //=input.value from main.js
-  const title = await fetch(providedUrl)
-    .then(function (res) {
-      const body = res.text();
-      console.log(body);
-      return body.split("<title>")[1].split("</title>")[0];
-    })
-    .catch((e) => {
-      console.log(e); //catch errors
-    });
-  res.send(title);
+  const providedUrl = req.body.URL; //=input.value from main.js
+  console.log(providedUrl);
+
+  const response = await got(providedUrl);
+
+  const dom = new JSDOM(response.body); //https://www.twilio.com/blog/web-scraping-and-parsing-html-in-node-js-with-jsdom
+  const allTitles = [...dom.window.document.querySelectorAll("title")];
+
+  let data = null;
+  allTitles.forEach((title) => {
+    //looping through title data
+    if (!data && title.text) {
+      data = title.text;
+    }
+  });
+  console.log(data);
+
+  res.json(data);
+
+  // const title = await fetch(providedUrl)
+  //   .then(function (res) {
+  //     const body = res.text();
+  //     console.log(body);
+  //     return body.split("<title>")[1].split("</title>")[0];
+  //   })
+  //   .catch((e) => {
+  //     console.log(e); //catch errors
+  //   });
 });
 
 // Listen for Requests
